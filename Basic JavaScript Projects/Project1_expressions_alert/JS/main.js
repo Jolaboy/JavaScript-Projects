43be8b385a5e9e905f9bb52c8d034ef6342061cc
Amadou Jarju <163463239+Jolaboy@users.noreply.github.com>
2025-03-11 14:56:49 +0000

    Adding Basic JavaScript folder
    Adding JavaScript folder containing files

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,34 @@
+// javascript file: main.js
+
+// window.alert() method Assignment
+window.alert("Hello, JavaScript"); // This will cause an alert window to pop up using window.alert() method
+
+// document.write() method Assignment
+document.write("Welcome to JavaScript Programming"); // The document.write() method can be used to make a text display
+
+// Variable Assignment
+var course = "JavaScript Programming";
+document.write(course);
+
+// using window.alert() method to display a variable
+var msg = "Hello, World!, This is the beginning JavaScript Programming";
+window.alert(msg);
+
+// Escape character Assignment
+document.write("one of the students asked the Tutor, \"Is JavaScript a Programming language?\\",
+    "the Tutor replied yes.");
+
+// Concatenation Assignment
+var fullname = "Amadou" + " " + "Jarju"
+document.write(fullname);
+
+// Multiple Variable Assignment
+var Family = "The Jarjus", Dad = "Kawsu", Mom = "Amie",
+    Daughter = "Fatou", Son = "Amadou";
+document.write(Dad); // invoked variable Dad
+
+// Expression Assignment
+var A = 10; // variable A assigned the value 10
+var B = 20; // variable B is assigned the value 20
+var sum = A + B; // variable sum will hold the values A and B
+document.write(A+B); // This will output the sum
